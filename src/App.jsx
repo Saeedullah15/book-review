14b1d1bf34784c327e6088d80b5d011f62450ff2
Saeedullah15 <saeedullahazim65@gmail.com>
2024-07-07 11:14:20 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1 className='text-red-500 text-4xl'>book review is loading....</h1>
+        </>
+    )
+}
+
+export default App
